62155e197d5605b57f9772d5d0172a889ffc7d50
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-08 06:48:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index do |array|
-    puts "#{array}"
+  array.each_with_index do
+    puts 
   end
 end
